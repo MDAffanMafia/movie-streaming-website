9a7de5c13a0b871e5069e67d552d7d201c4381a6
shaikhmudassir <skmudassirahmed@gmail.com>
2021-10-30 19:47:57 +0530

    logo added

--- client.php
+++ client.php
@@ -14,7 +14,7 @@ session_start();
      <header>
 
          <nav class="navbar navbar-expand-md navbar-dark bg-dark">
-             <a href="homepage.php" class="navbar-brand"> <img src="images/logo.png" alt=""> </a>
+             <a href="homepage.php" class="navbar-brand"> <img src="imgfile/logo.png" alt=""> </a>
              <span class="navbar-text">selfpromoMovie HUB</span>
 
              <ul class="navbar-nav">
